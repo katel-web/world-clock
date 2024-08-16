159dadae40ce82ef1b2eb2df425227b23a3fb527
katel-web <katelemass@gmail.com>
2024-08-16 15:07:12 +0100

    added current location to select options

--- javascript/index.js
+++ javascript/index.js
@@ -30,6 +30,10 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   console.log(cityTimezone);
   let cityTime = moment().tz(cityTimezone);
